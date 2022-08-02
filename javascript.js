6dad1b5f3ad600ab2fb44c0035b8c0a80d0c2779
Matt Dorchak <matthewdorchak235@gmail.com>
2022-08-01 23:57:25 -0400

    Setup files and create basic four functions

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+num1 = 16
+num2 = 25
+console.log(add(num1, num2))
+console.log(subtract(num1, num2))
+console.log(multiply(num1, num2))
+console.log(divide(num1, num2))
